7beec4f78fa17c81598f7bb13f73e53784a909d7
gymreklab <melissa.gymrek@gmail.com>
2018-09-10 11:58:33 -0700

    adding coloc files

---
+++ gwas/get_coloc_files_astle_bloodtraits.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#TISSUE=WholeBlood
+#GENE=ENSG00000147852.11
+#CHROM=9
+#SUMSTATS=/storage/mgymrek/gtex/gwas/summarystats/hgb_N172925_wide_form.tsv.gz
+#NAME=VLDLR
+#tmpdir=tmp
+
+TISSUE=$1
+GENE=$2
+CHROM=$3
+SUMSTATS=$4
+NAME=$5
+tmpdir=$(mktemp -d)
+
+# coloc package needs:
+# beta
+# varbeta
+
+echo $tmpdir
+
+# Extract beta and pvals for gtex/gwas
+cat /storage/szfeupe/Runs/650GTEx_estr/Analysis_by_Tissue/${TISSUE}/SNP_Analysis/Lin_Reg_Out | grep -w ${GENE} | cut -f 5,10,11 | sort -k1,1g > ${tmpdir}/gtex_locs.txt
+start=$(cat ${tmpdir}/gtex_locs.txt | datamash min 1)
+end=$(cat ${tmpdir}/gtex_locs.txt | datamash max 1)
+zcat ${SUMSTATS} | awk -v "chrom=$CHROM" '($4==chrom)' | \
+    awk -v"start=$start" -v "end=$end" '($5>=start && $5<=end)' | \
+    cut -f 5,11,12,16 | sort -k1,1g > ${tmpdir}/gwas_locs.txt
+echo "snppos,gwas.beta,gwas.varbeta,gwas.maf,gtex.beta,gtex.varbeta" | sed 's/,/ /g' > ${NAME}_coloc.txt
+join tmp/gwas_locs.txt tmp/gtex_locs.txt | \
+    awk -F" " '{print $1 " " $2 " " $3**2 " " $4 " " $5 " " $6**2}' >> ${NAME}_coloc.txt
--- gwas/readme.sh
+++ gwas/readme.sh
@@ -3,10 +3,24 @@
 # Get GWAS overlaps
 ./get_gwas_overlaps.sh
 
+# Filter gwas tables
+./filter_gwas.sh GWASCAT
+
 # VLDLR
 ./run_lz.sh 9 2622147 WholeBlood ENSG00000147852.11 100000
 ./run_lz_gwas.sh /storage/mgymrek/gtex/gwas/summarystats/hg_Astle_epacs.tab 9_2622147__WholeBlood_ld.txt rs369552432 rs2219143 VLDLR_HGB 2622147
+./get_coloc_files_astle_bloodtraits.sh WholeBlood ENSG00000147852.11 9 /storage/mgymrek/gtex/gwas/summarystats/hgb_N172925_wide_form.tsv.gz VLDLR
+Rscript --vanilla ./run_coloc_quant.R VLDLR_coloc.txt 172925
 
 # MED19 for SCZ
 ./run_lz.sh 11 57523883 Adipose-Subcutaneous ENSG00000156603.10 100000
 ./run_lz_gwas.sh /storage/mgymrek/gtex/gwas/summarystats/pgc_scz2_epacs.tab 11_57523883__Adipose-Subcutaneous_ld.txt rs9420 chr11:57523882 MED19_SCZ2 57523883
+
+# LCAT for HDL
+./run_lz.sh 16 68014740 WholeBlood ENSG00000213398.3 100000 
+./run_lz_gwas.sh /storage/mgymrek/gtex/gwas/summarystats/pgc_scz2_epacs.tab 16_68014740__WholeBlood_ld.txt rs56303487 chr16:68014739 LCAT_SCZ2 68014740
+
+# DCLK3
+./run_lz.sh 3 36835922 Skin-NotSunExposed ENSG00000163673.6 100000
+./run_lz_gwas.sh /storage/mgymrek/gtex/gwas/summarystats/pgc_scz2_epacs.tab 3_36835922__Skin-NotSunExposed_ld.txt rs3732386 chr3:36835921 DCLK3_SCZ2 36835922
+
